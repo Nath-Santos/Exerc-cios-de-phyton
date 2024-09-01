a1b828e91829a1319b1ccb5eae19289714079c9c
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-01 19:24:11 -0300

    Update exec-09.py

--- exec-09.py
+++ exec-09.py
@@ -1,1 +1,13 @@
+ #Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário.
+
+
+Valor_salario_mensal = float(input("digite o salário: "))
+
+reajuste = float(input("digite o reajuste em percentual (de 0 a 10): "))
+
+print("O valor do salário após o reajuste é de: ",Valor_salario_mensal* (1+reajuste))
+
+
+
+
 
