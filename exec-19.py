15824c7a23767d8f4627658cf6c35859e9cfa4c6
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 15:30:31 -0300

    Update exec-19.py

--- exec-19.py
+++ exec-19.py
@@ -13,4 +13,21 @@ if nb > na and nb > na:
 print(f"O menor número digitado foi {menor}")
 print(f"O maior número digitado foi {maior}")
 
+#verificar se a solução abaixo está correta também, na primeira não está imprindo o nº maior e o nº menor
 
+
+na = int(input("Digite o primeiro valor:"))
+nb = int(input("Digite o segundo valor:"))
+nc = int(input("Digite o terceiro valor:"))
+maior = na
+if nb > na and nb > nc:
+    maior = nb
+if nc > na and nc >= nb:
+    maior = nc
+menor = na
+if nb < nc and nb < na:
+    menor = nb
+if nc <= nb and nc < na:
+    menor = nc
+print(f"O menor número digitado foi {menor}")
+print(f"O maior número digitado foi {maior}")
