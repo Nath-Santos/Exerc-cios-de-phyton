4be5704402144761cb69bf765d168243dcb7c7a0
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 10:51:22 -0300

    Update exec-17.py

--- exec-17.py
+++ exec-17.py
@@ -1,1 +1,9 @@
 #Ler as notas da 1a. e 2a. avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga se o aluno foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é aprovado). Escrever também a média calculada.
+
+nota1av = float(input('Primeira nota:'))
+
+nota2av = float(input('Segunda nota:'))
+
+media = nota1av + nota2av
+
+print("Média aritmética: ", media)
