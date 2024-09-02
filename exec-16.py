9973e74928c1c139d241e4a8668da30476707baa
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 10:45:34 -0300

    exec-16.py

--- exec-16.py
+++ exec-16.py
@@ -1,14 +1,23 @@
 #As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia, e R$ 1,00 se forem compradas pelo menos 12. Escreva um programa que leia o número de maçãs compradas, calcule e escreva o custo total da compra.
-maca =int(input("Quantidade de macas:"))
+print("Quantidade de macas compradas:")
+macas_compradas = int(input())
 
-if maca < 12
+print("Valor unitario de macas para compra de 6 unidades:")
+valor_unitario = float(input())
 
-soma=maca*1.30
+valor_total_compra_6_unidades = macas_compradas * valor_unitario 
 
-print("Valor a ser pago compra menor que 12 unidades:",soma)
+print("valor total compra 6unidades:" + str(valor_total_compra_6_unidades))
 
-else:
 
-soma=maca*1.00
 
-print("Valor a ser pago compra menor que 12 unidades:",soma)
+
+print("Quantidade de macas compradas:")
+macas_compradas = int(input())
+
+print("Valor unitario de macas para compra de 12 unidades:")
+valor_unitario = float(input())
+
+valor_total_compra_12_unidades = macas_compradas * valor_unitario 
+
+print("valor total compra 12unidades:" + str(valor_total_compra_12_unidades))
