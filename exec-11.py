d3b64baf09ef848166a6498e158ba7d83c353816
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-01 21:07:52 -0300

    exec-11.py

--- exec-11.py
+++ exec-11.py
@@ -1,4 +1,4 @@
-Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por mês, mais uma comissão também fixa de R$ 700,00 para cada carro vendido e mais 5% do valor das vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro vendido. Calcule e escreva o salário final do vendedor.
+#Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por mês, mais uma comissão também fixa de R$ 700,00 para cada carro vendido e mais 5% do valor das vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro vendido. Calcule e escreva o salário final do vendedor.
 
 
 Valor_fixo_salario_mensal = (input("digite o valor do salario: "))
