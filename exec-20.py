3cdfcd5db515f8f3b1291a3aabb04831b16c9e13
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 15:43:59 -0300

    Update exec-20.py

--- exec-20.py
+++ exec-20.py
@@ -1,1 +1,5 @@
 #Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente.
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite um número: "))
+numero3 = int(input("Digite um número: "))
+
