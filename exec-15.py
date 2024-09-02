3017c0dce404a1953275a57a6e9028a0d8c2bcf0
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 09:54:56 -0300

    exec-15.py

--- exec-15.py
+++ exec-15.py
@@ -1,2 +1,10 @@
 #Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo)
 
+numero = int(input("Digite um número: "))
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
