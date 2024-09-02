d30500647371bd6013e1800bd1c56e6f29057d17
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 09:47:12 -0300

    exec-14.py

--- exec-14.py
+++ exec-14.py
@@ -1,1 +1,10 @@
 # Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10!
+
+
+numero = int(input("Digite um número: "))
+
+if numero > 10:
+    print("É MAIOR QUE 10.")
+
+if numero < 10:
+    print("É MENOR QUE 10.")
