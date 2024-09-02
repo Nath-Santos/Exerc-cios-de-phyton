98cf0cd30361de3e158f26f637660721ffdcf9a4
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 15:28:32 -0300

    Update exec-19.py

--- exec-19.py
+++ exec-19.py
@@ -1,8 +1,16 @@
 #Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
 
-print("Escreva o primeiro numero")
-numero1 = int(input("Digite um número: "))
+na = int(input("Digite o primeiro valor:"))
+nb = int(input("Digite o segundo valor:"))
 
-print("Escreva o segundo numero")
-numero2 = int(input("Digite um número: "))
+maior = na
+if nb > na and nb > na:
+    maior > nb
+    
+if nb > na and nb > na:
+    menor = na
+
+print(f"O menor número digitado foi {menor}")
+print(f"O maior número digitado foi {maior}")
+
 
