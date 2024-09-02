a28e9a5ed6b00149a304031afee3f497b8a8a939
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-02 15:03:51 -0300

    Update exec-19.py

--- exec-19.py
+++ exec-19.py
@@ -1,2 +1,8 @@
 #Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
 
+print("Escreva o primeiro numero")
+numero1 = int(input("Digite um número: "))
+
+print("Escreva o segundo numero")
+numero2 = int(input("Digite um número: "))
+
