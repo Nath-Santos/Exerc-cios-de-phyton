04b4a183fe027fd75c288941ca56a7a60c69d99a
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-01 21:49:11 -0300

    exec-11.py

--- exec-11.py
+++ exec-11.py
@@ -1,14 +1,18 @@
 #Uma revendedora de carros usados paga a seus funcionários vendedores um salário fixo por mês, mais uma comissão também fixa de R$ 700,00 para cada carro vendido e mais 5% do valor das vendas por ele efetuadas. Escrever um algoritmo que leia o número de carros por ele vendidos, o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro vendido. Calcule e escreva o salário final do vendedor.
 
 
-Valor_fixo_salario_mensal = (input("digite o valor do salario: "))
+print("Quantidade de carros vendidos no mês:")
+carros_vendidos = int(input())
 
-Valor_fixo_comissão = input("digite o valor fixo da comissao: ")
+print("Valor total de vendas:")
+vendas_total = float(input())
 
-Valor_de_Carros_Vendidos= input("digite o valor de carros vendidos:")
+print("Valor do salario fixo mensal:")
+salario_fixo = float(input())
 
-Valor_vendas_carros= float(input("digite o percentual sobre as vendas de carros (de 0 a 5): "))
+extra_bonus_por_carro_vendido = carros_vendidos * 700
+bonus_percentual = (5/100) * vendas_total
 
-print("O valor final do salário com as comissões é de,Valor_fixo_salario_mensal* (1+Valor_fixo_comissão) * (1+Valor_vendas_carros))
-      
+Salario_final = salario_fixo + extra_bonus_por_carro_vendido + bonus_percentual
 
+print("Salario final:" + str(Salario_final))
