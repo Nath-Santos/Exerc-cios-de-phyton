c44952b449e7d767a4d6e1c3ae241cc5e7ba85da
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-04 12:56:53 -0300

    Update exec-21.py

--- exec-21.py
+++ exec-21.py
@@ -1,1 +1,10 @@
 #Ler a hora de início e a hora de fim de um jogo de Xadrez (considere apenas horas inteiras, sem os minutos) e calcule a duração do jogo em horas, sabendo-se que o tempo máximo de duração do jogo é de 24 horas e que o jogo pode iniciar em um dia e terminar no dia seguinte
+print("JOGO DE XADREZ")
+print("Hora de inicio da partida:")
+hora_inicio = int(input())
+
+print()
+
+print("Hora de termino da partida:")
+
+hora_final = int(input())
