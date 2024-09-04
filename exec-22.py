6626f2a96e08392c735da984002c703422917752
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-04 14:17:49 -0300

    Update exec-22.py

--- exec-22.py
+++ exec-22.py
@@ -1,4 +1,16 @@
 #A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de 50%. Escreva um algoritmo que leia o número de horas trabalhadas em um mês, o salário por hora e escreva o salário total do funcionário, que deverá ser acrescido das horas extras, caso tenham sido trabalhadas (considere que o mês possua 4 semanas exatas).
+print("Considerando o valor do salario de: R$ 2000")
+print()
+salario_mes = 2000
+
+print()
+um_dia = 24
+trinta_dias = 720
+print()
+
+print("V$ salario por hora trab:")
+salario_hora_trab = salario_mes / total_horas_trab_mes
+
 jornada_trab_sem = 40 
 print("Quant horas trabalhadas na semana:")
 print(jornada_trab_sem)
