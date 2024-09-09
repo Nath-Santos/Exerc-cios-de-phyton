7576aafa322c53191b8faee3fd7c978b7ea7ea17
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-09 16:16:01 -0300

    Update exec-22.py

--- exec-22.py
+++ exec-22.py
@@ -1,19 +1,26 @@
 #A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de 50%. Escreva um algoritmo que leia o número de horas trabalhadas em um mês, o salário por hora e escreva o salário total do funcionário, que deverá ser acrescido das horas extras, caso tenham sido trabalhadas (considere que o mês possua 4 semanas exatas).
-salario_mes = int
-hrs_trabalhadas_semana = int
-semanas_trabalhadas = int
-salario_por_hora = float
-extra_por_hrs_trabalhadas = (0.5 * salario_por_hora) * 10
+total_horas_trab_mes = 160
+
+print("digite a quant de hrs trab no mes:")
+horas_trab_mes = int(input())
+
+print("digite o valor do salario por hora trabalhada:")
+salario_por_hora_trab = float(input())
+
+hora_extra = horas_trab_mes - total_horas_trab_mes
 print()
-print("digite o valor do salário:")
-input()
-print("digite a quant de hrs trabalhadas por semana:")
-input()
-print("digite a quant de semanas trabalhadas:")
-input()
+salario_com_extra = ((50/100) * salario_por_horas_trab) * hora_extra
+salario_do_mes = horas_trab_mes * salario_por_hora_trab
+
 print()
-if hrs_trabalhadas_semana > 160
-Salario_mes_final = salario_mes + extra_por_hrs_trabalhadas
-else
-Salario_mes_final = salario_mes
-print("Salario_mes_final")
+if horas_trab_mes == total_horas_trab_mes:
+    salario_com_extra = salario_do_mes
+print("salario_do_mes:R$ " + str(salario_do_mes))
+print("salario_com_extra: R$ " + str(salario_extra))
+elif horas_trab_mes > total_horas_trab_mes:
+    salario_com_extra = salario_do_mes + salario_com_extra
+print("salario_do_mes:R$ " + str(salario_do_mes))
+print("salario_por_hora_extra R$ " + str(salario_com_extra))
+print("salario_com_extra: R$ " + str(salario_com_extra))
+else: salario_com_extra = salario_do_mes:
+print("salario_com_extra: R$ " + str(salario_com_extra))
