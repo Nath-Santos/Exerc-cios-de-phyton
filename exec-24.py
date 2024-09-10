811c9c6df287f9523ee8beb9594f1898d726a892
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 10:13:49 -0300

    Update exec-24.py

--- exec-24.py
+++ exec-24.py
@@ -8,3 +8,8 @@ valor_vendas_mes = float(input())
 comissao1_extra = (3/100) * valor_vendas_mes
 extra_ref_vendas = valor_vendas_mes - 1500
 comissao2_extra = (5/100) * extra_ref_vendas
+if valor_vendas_mes => 1500:
+   salario_mes_total = salario_fixo + comissao1_extra
+else:
+   salario_mes_total = salario_fixo + comissao1_extra + comissao2_extra
+   print("Salario do mes total R$" + str(salario_mes_total))
