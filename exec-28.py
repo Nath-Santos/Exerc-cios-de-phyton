b2435df6d6f6be6b4c3bd30aded8e5ef0c33efbe
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 13:58:52 -0300

    Create exec-28.py

---
+++ exec-28.py
@@ -0,0 +1,12 @@
+#Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
+
+print("Escreva o 1º numero")
+num1 =float(input())
+
+print("Escreva o 2º numero")
+num2 =float(input())
+
+print("Escreva o 3º numero")
+num3 =float(input())
+
+if
