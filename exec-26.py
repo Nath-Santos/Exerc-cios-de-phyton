0b699c62df96ee9b97373e34cf59c94b7a6c9959
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 13:47:05 -0300

    Update exec-26.py

--- exec-26.py
+++ exec-26.py
@@ -1,4 +1,5 @@
 #Faça um algoritmo para ler: quantidade atual em estoque, quantidade máxima em estoque e quantidade mínima em estoque de um produto. Calcular e escrever a quantidade média ((quantidade média = quantidade máxima + quantidade mínima)/2). Se a quantidade em estoque for maior ou igual a quantidade média, escrever a mensagem 'Não efetuar compra', senão escrever a mensagem 'Efetuar compra'.
+
 print("Controle de Estoque")
 
 print("Quantidade atual do produto")
@@ -11,5 +12,10 @@ print("Quantidade mínima do produto")
 quant_minima = int(input())
 
 quant_media = (quant_maxima + quant_minima) / 2
-
 print("Quantidade media do produto em estoque:" + str(quant_media))
+
+if  quant_atual >= quant_media:
+    print("Não efetuar a compra")
+
+else:
+    print("Eeftuar a compra")
