80e4d2db81adb52783a792ab1f5bd3438e588000
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 15:53:47 -0300

    Update  exec-31.py

--- exec-31.py
+++ exec-31.py
@@ -1,4 +1,5 @@
 #Ler 3 valores (A, B e C) representando as medidas dos lados de um triângulo e escrever se formam ou não um triângulo. OBS: para formar um triângulo, o valor de cada lado deve ser menor que a soma dos outros 2 lados.
+
 print("Escreva o valor do lado A")
 valorA  = float(input())
 
@@ -8,4 +9,8 @@ valorB = float(input())
 print("Escreva o valor do lado C")
 valorC = float(input())
 
-if valorA < valorB + valorC
+if  valorA < valorB + valorC and valorB < valorC + valorA and valorC < valorA + valorB:
+    print("As medidas formam um triangulo")
+
+else:
+     ("As medidas não formam um triangulo")
