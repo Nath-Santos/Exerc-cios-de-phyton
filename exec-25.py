cdb8982a89f739898b57c6a4abca109ab07cfe42
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 10:34:29 -0300

    Create exec-25.py

---
+++ exec-25.py
@@ -0,0 +1,22 @@
+#Faça um algoritmo para ler: número da conta do cliente, saldo, débito e crédito. Após, calcular e escrever o saldo atual (saldo atual = saldo - débito + crédito). Também testar se saldo atual for maior ou igual a zero escrever a mensagem 'Saldo Positivo', senão escrever a mensagem 'Saldo Negativo'.
+
+print("Dados da conta do cliente")
+print("Nº da conta:")
+numero_da_conta = int(input())
+
+print("Saldo da conta:")
+saldo_conta = float(input())
+
+print("Débito:")
+debito = float(input())
+
+print("Crédito:")
+crédito = float(input())
+
+Saldo_atual_da_conta = float(saldo_conta - debito + credito)
+print(Saldo_atual_da_conta)
+
+
+
+
+
