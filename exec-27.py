9530a76c45d857458b91b29c369fb93b3ccf8cfb
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 13:55:53 -0300

    Create exec-27.py

---
+++ exec-27.py
@@ -0,0 +1,12 @@
+#Ler um valor e escrever se é positivo, negativo ou zero.
+print("Escreva o valor")
+valor = float(input())
+
+if  valor > 0:
+    print("POSITIVO")
+    
+elif valor == 0:
+    print("ZERO")
+    
+else:
+    print("NEGATIVO")
