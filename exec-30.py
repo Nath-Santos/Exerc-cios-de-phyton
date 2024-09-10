d486e99078bd38f6d4e09407878fe0c762133fdc
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-10 15:34:41 -0300

    Create exec-30.py

---
+++ exec-30.py
@@ -0,0 +1,15 @@
+#Ler 3 valores (considere que não serão informados valores iguais) e escrevê-los em ordem crescente.
+print("Escreva o 1º numero")
+num1 = float(input())
+
+print("Escreva o 2º numero")
+num2 = float(input())
+
+print("Escreva o 3º numero")
+num3 = float(input())
+
+numeros = [num1, num2, num3]
+
+numeros.short()
+  
+print("Segue os números em ordem crescente:" + str(numeros))
