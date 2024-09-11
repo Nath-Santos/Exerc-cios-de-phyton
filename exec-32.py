6d68a428db2eff5c14964868494e3857d734475f
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-11 16:28:57 -0300

    Update exec-32.py

--- exec-32.py
+++ exec-32.py
@@ -1,1 +1,18 @@
 #Ler o nome de 2 times e o número de gols marcados na partida (para cada time). Escrever o nome do vencedor. Caso não haja vencedor deverá ser impressa a palavra EMPATE.
+print("Time 1")
+num_gols_tm1 =int(input())
+
+print("Time 2")
+num_gols_tm2 =int(input())
+
+
+if   num_gols_tm1 > num_gols_tm2:
+     print(num_gols_tm1)
+
+elif num_gols_tm1 == num_gols_tm2:
+     print(num_gols_tm2)
+
+
+   
+  
+print("A soma dos maiores valores é igual a:" + str(soma))
