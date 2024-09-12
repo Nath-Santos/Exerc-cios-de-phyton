6b236bfe6dc7c2a8673ebd006dbe372f43c00c21
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-12 14:39:08 -0300

    Update exec-34.py

--- exec-34.py
+++ exec-34.py
@@ -1,1 +1,19 @@
 #dado o algoritmo, faça um teste de mesa  que complete o quadro  a seguir:
+print("Escreva o valor do lado X")
+valorX  = float(input())
+
+print("Escreva o valor do lado Y")
+valorY = float(input())
+
+
+
+valorZ = (valorX * valorY) + 5
+
+if   valorZ <= 0:
+   print("A" +str(valorZ))
+   
+elif valorZ <= 100:
+   print("B" +str(valorZ))
+   
+else:
+    print("C")
