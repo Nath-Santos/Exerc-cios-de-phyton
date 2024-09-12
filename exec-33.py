4036245d80ab699db13f613d06555138341cae5a
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-12 10:56:47 -0300

    Update exec-33.py

--- exec-33.py
+++ exec-33.py
@@ -3,3 +3,23 @@
 #‘Números iguais’, caso os números sejam iguais
 #‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
 #‘Segundo maior’, caso o segundo seja maior que o primeiro.
+
+print("Escreva o 1º numero")
+num1 =float(input())
+
+print("Escreva o 2º numero")
+num2 =float(input())
+
+print("Escreva o 3º numero")
+num3 =float(input())
+
+if num1 == num2 and num1 == num3:
+   print("NÚMEROS IGUAIS")
+
+elif num1 < num2:
+   print("PRIMEIRO É MAIOR")
+
+else:
+   num2 > num1
+  
+print("SEGUNDO É MAIOR")
