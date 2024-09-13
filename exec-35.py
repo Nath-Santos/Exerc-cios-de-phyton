0f30d3edd3cbd56c1cc823ec0d122f4ca81d800e
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-13 09:31:53 -0300

    Create exec-35.py

---
+++ exec-35.py
@@ -0,0 +1,1 @@
+#Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90. 
