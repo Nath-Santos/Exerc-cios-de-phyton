14076926db8b94278895dd68745b75bdb4a1993c
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-14 20:05:40 -0300

    Update exec-35.py

--- exec-35.py
+++ exec-35.py
@@ -1,25 +1,27 @@
 #Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90. 
+#Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90. 
 print("Nº de litros Vendidos: ")
 quant_litros = int(input())
 
 print("Código do combustivel, sendo A = ALCOOL E G = GASOLINA")
-cod_combustivel = str(input())
+cod_comb = str(input())
 
-if cod_combustivel == "Alcool":
-   cod_combustivel = "A"
-
-else:    
-if cod_combustivel == "Gasolina":
-   cod_combustivel = "G"
+if cod_comb == "Alcool":
+   cod_comb = "A"
    
-if quant_litros > 20 and cod_combustivel == "A":
-   pag_preco_final = (quant_litros * 2.90) - ((5 / 100) * cod_combustivel)
-elif quant_litros > 20 and cod_combustivel == "G":
-   pag_preco_final = (quant_litros * 3.30) - ((6 / 100) * quant_litros)
+else:    
+     if cod_comb == "Gasolina":
+        cod_comb = "G"
+   
+if quant_litros > 20 and cod_comb == "A":
+   pag_preco_final = (quant_litros * 2.90) - ((5 / 100) * quant_litros)
+   
+elif quant_litros > 20 and cod_comb == "G":
+     pag_preco_final = (quant_litros * 3.30) - ((6 / 100) * quant_litros)
 elif quant_litros <= 20 and cod_combustivel == "A":
-   pag_preco_final = (quant_litros * 2.90) - ((3 / 100) * quant_litros)
+     pag_preco_final = (quant_litros * 2.90) - ((3 / 100) * quant_litros)
 else:
-    if quant_litros <= 20 and cod_combustivel == "G":
-   pag_preco_final = (quant_litros * 3.30) - ((4 / 100) * quant_litros)
+     if quant_litros <= 20 and cod_comb == "G":
+        pag_preco_final = (quant_litros * 3.30) - ((4 / 100) * quant_litros)
 
 print("O valor do preco final para pagamento: R$" + str(pag_preco_final)) 
