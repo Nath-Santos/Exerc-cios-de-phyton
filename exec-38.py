aa98a9ee9bccf4a0243e15ae377f0da851d4689d
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-14 20:58:11 -0300

    Update exec-38.py

--- exec-38.py
+++ exec-38.py
@@ -1,5 +1,4 @@
-#38) Faça um algoritmo para ler um número que é um código de usuário. Caso este código seja diferente de um código armazenado internamente no algoritmo (igual a 1234) deve ser apresentada a mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’. Caso a senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’.
-
+#38) Faça um algoritmo para ler um número que é um código de usuário. Caso este código seja diferente de um código armazenado internamente no algoritmo (igual a 1234) deve ser apresentada a mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’. Caso a senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’. 
 print("Código do Usuário: ")
 cod_usuario = int(input())
 
@@ -7,11 +6,14 @@ cod_usuario = int(input())
 cod_correto = 1234
 senha_correta = 9999
 
-if  cod_usuario != cond_correto
+if  cod_usuario != cod_correto:
     print("Usuário invalido!")
 
 else:
-    if senha_usuario = int(input("Digite sua senha:"))
+    if senha_usuario = int(input("Digite sua senha:  "))
     
-    if senha_usuario != senha_correta
+    if senha_usuario != senha_correta:
     print("Senha incorreta!")
+    
+else:
+    print("Acesso permitido")
