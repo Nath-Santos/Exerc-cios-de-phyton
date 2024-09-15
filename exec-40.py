a00caf06099437fb92e3273cb9cb77eb1d2dd5ba
Nathália Santos <nathmarcalsantos@gmail.com>
2024-09-14 22:56:55 -0300

    Update exec-40.py

--- exec-40.py
+++ exec-40.py
@@ -4,22 +4,30 @@
 #- Se quantidade > 5 e quantidade <=10 o desconto será de 3%
 #- Se quantidade > 10 o desconto será de 5%
 
-def calculo_do_total(nome_produto, quant_produto, preco_produto):
-total_produto = quant_produto * preco_produto
+def calculo_do_total(nome_produto,quantidade_adquirida, preco_unitario):
+total_produto = quantidade_adquirida * preco_unitario
 
-if quant_produto <= 5:
+if quantidade_adquirida <= 5:
    desconto = total_produto * 0.02
-elif quant_produto <= 10:
+elif quantidade_adquirida <= 10:
      desconto = total_produto * 0.03
     else:
-     desconto = total_produto * 0.05
+        desconto = total_produto * 0.05
 
-valor_tt_p_pagamento n= total_produto - desconto
+valor_tt_p_pagamento = total_produto - desconto
 
-return total_produto, descontovalor_tt_p_pagamento
+return total_produto, desconto, valor_tt_p_pagamento
 
 nome_produto = "Produto"
 quantidade_adquirida = 8
 preco_unitario = 10.0
 
 total_produto, desconto, total_a_pagar = calculo_do_total(nome_produto, quantidade_adquirida, preco_unitario)
+
+print(f"Produto: {nome_produto}")
+print(f"Quantidade adquirida: {quantidade_adquirida}")
+print(f"Preço unitário: {preco_unitario}")
+print(f"Total_produto: {total_produto}")
+print(f"Desconto: {desconto}")
+print(f"Valor_tt_p_oagamento: {valor_tt_p_pagamento}")
+
