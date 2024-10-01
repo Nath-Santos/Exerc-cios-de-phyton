5bb0de3bee4c40bbb8d6f774d3b00739d996344f
Nathália Santos <nathmarcalsantos@gmail.com>
2024-10-01 08:15:06 -0300

    exec-41.py

---
+++ exec-41.py
@@ -0,0 +1,10 @@
+# 41) Faça um algoritmo para ler as 3 notas obtidas por um aluno nas 3 verificações e a média dos exercícios que fazem parte da avaliação. Calcular a média de aproveitamento, usando a fórmula abaixo e escrever o conceito do aluno de acordo com a tabela de conceitos mais abaixo:
+
+# Média_de_aproveitamento = (N1 + (N2 * 2) + (N3 * 3)) / 7 
+
+# A atribuição de conceitos obedece a tabela abaixo:
+
+# A > = 9,0
+# B > = 7,5 e < 9,0
+# C > = 6,0 e < 7,5
+# D < 6,0
