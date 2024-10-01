e17eb8a76c4e4e98747a8d31f14fa48361feaeea
Nathália Santos <nathmarcalsantos@gmail.com>
2024-10-01 08:21:07 -0300

    exec-41.py

--- exec-41.py
+++ exec-41.py
@@ -8,3 +8,31 @@
 # B > = 7,5 e < 9,0
 # C > = 6,0 e < 7,5
 # D < 6,0
+
+print("Escreva a nota da 1ª Avaliação:")
+N1 = float(input())
+
+print("Escreva a nota da 2ª Avaliação:")
+N2 = float(input())
+
+print("Escreva a nota da 3ª Avaliação:?")
+N3 = float(input())
+
+Média_de_aproveitamento = (N1 + (N2 * 2) + (N3 * 3)) / 7
+
+if Média_de_aproveitamento >= 9:
+    Conceito = "A"
+    print(f"Media de aproveitamento: {Média_de_aproveitamento:.2f}")
+    print(f"Conceito: {Conceito}")
+elif Média_de_aproveitamento >= 7.5 and Média_de_aproveitamento < 9:
+    Conceito = "B"
+    print(f"Media de aproveitamento: {Média_de_aproveitamento:.2f}")
+    print(f"Conceito: {Conceito}")
+elif media_aproveitamento >= 6 and Média_de_aproveitamento < 7.5:
+    Conceito = "C"
+    print(f"Media de aproveitamento: {Média_de_aproveitamento:.2f}")
+    print(f"Conceito: {Conceito}")
+else:
+    Conceito = "D"
+    print(f"Media de aproveitamento: {Média_de_aproveitamento:.2f}")
+    print(f"Conceito: {Conceito}")
